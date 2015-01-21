a4b9c59de08b9645981b083b64be1ce2c667b181
304471720 <lijia-wy@163.com>
2015-01-21 12:05:28 +0800

    test

--- ljj_shell_tools.sh
+++ ljj_shell_tools.sh
@@ -27,5 +27,36 @@ fi
 #echo "1--------------"
 echo $result
 #echo "2--------------"
-return $result
+#return "$result"
 }
+
+#result1=$(GetMemcachedStatsValuesByKeys '192.168.5.172 11204' 'bytes_written limit_maxbytes curr_items total_items')
+#echo $result1
+
+SendMsg()
+{
+	if [ $# != 5 ] ; then
+		echo "USAGE: $0 ' phone number '   'msg content'  'msg url' 'account' 'password' " 
+		echo " e.g.: $0 131xxxxxxxx   "testtest"  'xxx.xxx.xxx/xx/xx/xxxx.jsp' xxxx xxxxxx  " 
+		exit 1;
+	fi
+        tel=$1
+        msgcont=$(eval echo $2 | iconv -f gb2312 -t utf8)
+        r=`curl  -s "http://$3?name=$4&pwd=$5&dst=$tel" -d "&msg=$msgcont" | awk '{printf $1}'`
+        if [ "$r"x == "0000"x ]; then
+                echo "send sms success!"
+		#return 0
+        else
+		echo "send sms fails!"
+		#$return 1
+        fi
+        #echo  $tel $msgcont
+}
+
+#SendMsg 13121309305   "testtest"  'wireless.soufun.com/sms_validate/sendsms.do' test test 
+SendMsg 13121309305   "≤‚ ‘≤‚ ‘"  'wireless.soufun.com/sms_validate/sendsms.do' test test  
+
+
+
+
+
